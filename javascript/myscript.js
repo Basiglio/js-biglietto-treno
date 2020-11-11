200fcf176ab2069d7fc0d196ce562e5d2784eada
Emilo <emilio.furnari87@gmail.com>
2020-11-11 16:03:30 +0100

    w le espressioni -.-

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,9 +1,3 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km);
-// va applicato uno sconto del 20% per i minorenni;
-// va applicato uno sconto del 40% per gli over 65.
-
 
 // chiedo i km
 var km = prompt("quanti km devi percorrere?")
@@ -11,23 +5,26 @@ var km = prompt("quanti km devi percorrere?")
 // chiedo l'età
 var anni = prompt("quanti anni hai?")
 
-// definisco prezzo
+// // definisco prezzo
 var prezzo = 0.21
 
+// prezzo senza sconto
+var prezzoTot = km * prezzo
 
-// calcolo prezzo
+// // calcolo sconto
 if (anni < 18) {
-  // prezzo biglietto minorenne scontato
-  var prezzoBiglietto = ((km * prezzo) * 20 / 100);
+ // sconto minorenne
+ var prezzoBiglietto = (prezzoTot - ((prezzoTot * 20) / 100));
+
 } else if (anni >= 65) {
-  // prezzo biglietto over 65
-  var prezzoBiglietto = ((km * prezzo) * 40  / 100);
+ // sconto over 65
+ var prezzoBiglietto = (prezzoTot - ((prezzoTot * 40) / 100));
+
 }
 else  {
-  // prezzo biglietto normale
-  var prezzoBiglietto = (km * prezzo + " €");
-}
+ // nessuno sconto
+  var prezzoBiglietto = (km * prezzo);
+ }
 
-
-// visualizzo prezzo senza sconto
+// visualizzo prezzo
 document.getElementById("prezzo_biglietto").innerHTML = prezzoBiglietto
