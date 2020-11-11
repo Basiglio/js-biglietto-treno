beb589f67b3eb01d824ea76c77f2e1265731e022
Emilo <emilio.furnari87@gmail.com>
2020-11-11 16:18:31 +0100

    testo l'arrotondamento

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -8,23 +8,26 @@ var anni = prompt("quanti anni hai?")
 // // definisco prezzo
 var prezzo = 0.21
 
-// prezzo senza sconto
+// prezzo effettivo
 var prezzoTot = km * prezzo
 
-// // calcolo sconto
+// calcolo sconto
 if (anni < 18) {
  // sconto minorenne
  var prezzoBiglietto = (prezzoTot - ((prezzoTot * 20) / 100));
-
 } else if (anni >= 65) {
  // sconto over 65
  var prezzoBiglietto = (prezzoTot - ((prezzoTot * 40) / 100));
-
 }
 else  {
  // nessuno sconto
-  var prezzoBiglietto = (km * prezzo);
- }
+ var prezzoBiglietto = (km * prezzo);
+}
+
+// arrotondamento
+var prezzoArrotondato = Math.floor(prezzoBiglietto)
+
+
 
 // visualizzo prezzo
-document.getElementById("prezzo_biglietto").innerHTML = prezzoBiglietto
+document.getElementById("prezzo_biglietto").innerHTML = prezzoArrotondato
